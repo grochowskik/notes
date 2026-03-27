a734f99aa6897e022d4d7138c4c3b00773530b54
Krzysztof Grochowski <krzysztofgrochowski3@gmail.com>
2026-03-27 11:55:15 +0100

    Added migrations for users and notes

---
+++ backend/migrations/000001_create_notes_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS notes (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    version integer NOT NULL DEFAULT 1,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
